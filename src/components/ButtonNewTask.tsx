9ad1f7ddb0f1603d1069361f872b5ace7f120c1b
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2021-12-06 23:16:27 +0700

    feat: add todo

--- src/components/ButtonNewTask.tsx
+++ src/components/ButtonNewTask.tsx
@@ -1,6 +1,21 @@
-export default function ButtonNewTask() {
+import { TodoType } from "@/types";
+import { Dispatch, SetStateAction } from "react";
+
+interface ButtonNewTaskProps {
+  setTodos: Dispatch<SetStateAction<TodoType[]>>;
+}
+
+export default function ButtonNewTask({ setTodos }: ButtonNewTaskProps) {
+  const addNewTodo = () => {
+    const newTodo = { id: Date.now(), title: "new todo", status: 0 };
+    setTodos((crr) => [...crr, newTodo]);
+  };
+
   return (
-    <button className="relative -top-6 bg-purple-500 hover:bg-purple-600 font-medium text-white rounded-3xl py-3 px-6">
+    <button
+      onClick={addNewTodo}
+      className="relative -top-6 bg-purple-500 hover:bg-purple-600 font-medium text-white rounded-3xl py-3 px-6"
+    >
       + New task
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const Home: NextPage = () => {
           </ul>
         </div>
         <div className="flex justify-center">
-          <ButtonNewTask />
+          <ButtonNewTask setTodos={setTodos} />
         </div>
       </div>
     </div>
@@ -33,29 +33,9 @@ const Home: NextPage = () => {
 export default Home;
 
 const defaultTodos = [
-  {
-    id: 1,
-    title: "Make a meal",
-    status: 1,
-  },
-  {
-    id: 2,
-    title: "Dinner with family",
-    status: 0,
-  },
-  {
-    id: 3,
-    title: "Watch scary movie",
-    status: 0,
-  },
-  {
-    id: 4,
-    title: "Learn something new",
-    status: 1,
-  },
-  {
-    id: 5,
-    title: "Make a phone call to mom",
-    status: 1,
-  },
+  { id: 1, title: "Make a meal", status: 1 },
+  { id: 2, title: "Dinner with family", status: 0 },
+  { id: 3, title: "Watch scary movie", status: 0 },
+  { id: 4, title: "Learn something new", status: 1 },
+  { id: 5, title: "Make a phone call to mom", status: 1 },
 ];
